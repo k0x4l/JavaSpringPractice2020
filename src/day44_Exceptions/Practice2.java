6bd34e9823ac695ff07343c49f5b1553920ec2a3
Koksal Ogec <ogeckoksal@gmail.com>
2020-05-29 11:38:04 -0400

    05-29

---
+++ src/day44_Exceptions/Practice2.java
@@ -0,0 +1,14 @@
+package day44_Exceptions;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        System.out.println("java");
+        waits(5.5);
+        System.out.println("Cybertek");
+    }
+    public static void waits (double seconds){
+        try {
+            Thread.sleep((long)(seconds*1000));
+        } catch (Exception e) { }
+    }
+}
