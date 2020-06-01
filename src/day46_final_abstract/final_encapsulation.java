3bfb2bf8c483d8c4f980ab2ab7b806d3133a3d3b
Koksal Ogec <ogeckoksal@gmail.com>
2020-06-01 10:46:15 -0400

    05-28_2

--- src/day46_final_abstract/final_encapsulation.java
+++ src/day46_final_abstract/final_encapsulation.java
@@ -1,4 +1,15 @@
 package day46_final_abstract;
 
 public class final_encapsulation {
+    final private int data1 = 200;
+    private int data2 = 300;
+
+    public int getData1(){
+        return data1;
+    }
+    /*
+    public void setData1(int data1){
+        this.data1 = data1 ;
+    }
+     */
 }
